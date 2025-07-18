c8a62acde5ba7fc3dbe4a993cba030b727db0c99
masifafzal81 <masifafzal81@email.com>
2025-07-18 09:31:09 +0500

    login and logout feature

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     
 ]
--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    
+    # check the logged in user
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # authenticate the user
+    
+        user = authenticate(request, username=username, password=password)
+    
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        
+        else:
+            messages.success(request, 'Invalid username or password')
+            return redirect('home')        
+    
+    else:
+        return render(request, 'home.html', {})
+
+# def login_view(request):
+#     pass
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You are successfully logged out.')
+    return redirect('home')
